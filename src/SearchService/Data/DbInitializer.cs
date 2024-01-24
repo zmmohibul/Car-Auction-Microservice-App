f5a06a74c03828e76ec459e2e37cf5163bc75567
zmmohibul <18-36427-1@student.aiub.edu>
2024-01-24 23:52:03 +0600

    Search Service

---
+++ src/SearchService/Data/DbInitializer.cs
@@ -0,0 +1,41 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Data;
+
+public class DbInitializer
+{
+    public static async Task InitDb(WebApplication app)
+    {
+        await DB.InitAsync("SearchDb", MongoClientSettings
+            .FromConnectionString(app.Configuration.GetConnectionString("MongoDbConnection")));
+
+        await DB.Index<Item>()
+            .Key(x => x.Make, KeyType.Text)
+            .Key(x => x.Model, KeyType.Text)
+            .Key(x => x.Color, KeyType.Text)
+            .CreateAsync();
+
+        var count = await DB.CountAsync<Item>();
+
+        // using var scope = app.Services.CreateScope();
+        //
+        // var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
+        //
+        // var items = await httpClient.GetItemsForSearchDb();
+        //
+        // Console.WriteLine(items.Count + " returned from the auction service");
+
+        if (count == 0)
+        {
+            Console.WriteLine("Adding data...");
+            var itemData = await File.ReadAllTextAsync("Data/auctions.json");
+            var options = new JsonSerializerOptions{ PropertyNameCaseInsensitive = true};
+            var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+            await DB.SaveAsync(items);
+        }
+    }
+}
---
+++ src/SearchService/Program.cs
@@ -0,0 +1,39 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Data;
+using SearchService.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+    await DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+
+app.Run();
